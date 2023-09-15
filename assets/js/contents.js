996ee6f544dae028dc82576b3fccc3a068b13462
sharawaa <115978040+sharawaa@users.noreply.github.com>
2023-09-15 09:20:10 +0000

    gsap animation

---
+++ assets/js/contents.js
@@ -0,0 +1,98 @@
+const block_1= document.querySelector('.block_1')
+const block_2= document.querySelector('.block_2')
+const block_3= document.querySelector('.block_3')
+
+const img_1= document.querySelector('.image_1')
+const img_2= document.querySelector('.image_2')
+const img_3= document.querySelector('.image_3')
+
+gsap.registerPlugin(ScrollTrigger);
+
+// block 1
+gsap.to(block_1,{
+    x:100, 
+    duration:3, 
+    scrollTrigger:{
+        trigger:block_1,
+        start:"top 80%",
+        end:"top 30%",
+        scrub:4,
+        // markers:true
+    }
+})
+gsap.to(img_1,{
+    x:-100, 
+    duration:3, 
+    scrollTrigger:{
+        trigger:img_1,
+        start:"top 80%",
+        end:"top 30%",
+        scrub:4,
+        // markers:true
+    }
+})
+// block 2
+gsap.to(block_2,{
+    x:-100, 
+    duration:3, 
+    scrollTrigger:{
+        trigger:block_2,
+        start:"top 80%",
+        end:"top 30%",
+        scrub:4,
+        // markers:true
+    }
+})
+gsap.to(img_2,{
+    x:100, 
+    duration:3, 
+    scrollTrigger:{
+        trigger:img_2,
+        start:"top 80%",
+        end:"top 30%",
+        scrub:4,
+        // markers:true
+    }
+})
+
+// block 3
+gsap.to(block_3,{
+    x:100, 
+    duration:3, 
+    scrollTrigger:{
+        trigger:block_3,
+        start:"top 80%",
+        end:"top 30%",
+        scrub:4,
+        // markers:true
+    }
+})
+gsap.to(img_3,{
+    x:-100, 
+    duration:3, 
+    scrollTrigger:{
+        trigger:img_3,
+        start:"top 80%",
+        end:"top 30%",
+        scrub:4,
+        // markers:true
+    }
+})
+gsap.to(".footer_right",{y:-100, duration:3,
+scrollTrigger:{
+    trigger:'.footer_right',
+// markers:true,
+start:"top 80%",
+end:'top 30%',
+scrub:4,
+
+}})
+gsap.to(".footer_left",{y:-100, x:100,  duration:3,
+    scrollTrigger:{
+        trigger:'.footer_right',
+    // markers:true,
+    start:"top 80%",
+    end:'top 30%',
+    scrub:4,
+    
+    }})
