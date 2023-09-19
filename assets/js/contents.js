989fcad5a936ba3d415f441681dff28251707636
Sharawmnyambuu <sharawnyambuuurtnasan@gmail.com>
2023-09-19 09:47:35 +0800

    active

--- assets/js/contents.js
+++ assets/js/contents.js
@@ -1,5 +1,37 @@
 
+// const a = document.querySelectorAll('.quick ul li a')
+const ac1 = document.querySelector('.unActive1')    
+const ac2 = document.querySelector('.unActive2')    
+const ac3 = document.querySelector('.unActive3')    
+const ac4 = document.querySelector('.unActive4')    
+const ac5 = document.querySelector('.unActive5')    
 
+    window.addEventListener('scroll', () => {
+        const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const scrollPercentage = (scrollTop / scrollableHeight) * 100;
+    console.log('test',scrollPercentage,'%');
+    
+    if(scrollPercentage < 20){
+        ac1.style.borderColor= '#ffffff'
+        ac2.style.borderColor= '#ffffff80'
+    }if(20 < scrollPercentage < 40){
+        ac2.style.borderColor= '#ffffff'
+        ac1.style.borderColor= '#ffffff80'
+        ac3.style.borderColor= '#ffffff80'
+    }if(40<scrollPercentage < 70){
+        ac2.style.borderColor= '#ffffff80'
+        ac3.style.borderColor= '#ffffff'
+        ac4.style.borderColor= '#ffffff80'
+    }if(70<scrollPercentage < 95){
+        ac3.style.borderColor= '#ffffff80'
+        ac4.style.borderColor= '#ffffff'
+        ac5.style.borderColor= '#ffffff80'
+    }if(95<scrollPercentage){
+        ac4.style.borderColor= '#ffffff80'
+        ac5.style.borderColor= '#ffffff'
+    }
+      });
 
 function scrollTriggerFunction(element, x, y, start, end) {
     gsap.to(element, {
